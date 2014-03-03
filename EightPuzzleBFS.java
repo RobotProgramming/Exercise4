d070d78832aa5ad0a3158ab603ffd686e1a28bd0
charliechambers <cxc320@bham.ac.uk>
2014-03-03 21:34:06 +0000

    Create EightPuzzleBFS.java

---
+++ EightPuzzleBFS.java
@@ -0,0 +1,36 @@
+package exercise4;
+import java.util.ArrayList;
+import java.util.List;
+
+import rp13.search.problem.puzzle.EightPuzzle;
+import rp13.search.problem.puzzle.EightPuzzle.PuzzleMove;
+import rp13.search.problem.puzzle.EightPuzzleSuccessorFunction;
+import rp13.search.util.ActionStatePair;
+
+public class EightPuzzleBFS extends BFS<PuzzleMove, EightPuzzle> {
+	public EightPuzzleBFS() {
+		super(EightPuzzle.randomEightPuzzle(), 
+			EightPuzzle.orderedEightPuzzle(),
+			new EightPuzzleSuccessorFunction());
+	}
+
+	public static void main(String[] args) {
+		EightPuzzleBFS searcher = new EightPuzzleBFS();
+		Node<PuzzleMove, EightPuzzle> node = searcher.search();
+
+		List<ActionStatePair<PuzzleMove, EightPuzzle>> path = new ArrayList<ActionStatePair<PuzzleMove,EightPuzzle>>();
+		while(node != null) {
+			path.add(0, node.getActionStatePair());
+			node = node.getParent();
+		}
+		for(ActionStatePair<PuzzleMove, EightPuzzle> pathItem : path) {
+			if(pathItem.getAction() == null) {
+				System.out.print("Start:\n" + pathItem.getState());
+			} else {
+				System.out.print(pathItem.getAction() + ": \n" + pathItem.getState());
+			}
+			System.out.println();
+		}
+		System.out.println(path.toArray().length);
+	}
+}
